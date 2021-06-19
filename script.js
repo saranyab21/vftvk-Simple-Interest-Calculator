6e9937768e444c6caaf395ea2b5dcefd41daffa9
Saranya Bhattacharjee <79959032+saranyab21@users.noreply.github.com>
2021-06-20 00:15:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
+function compute()
+{
+    p = document.getElementById("principal").value;
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
-    var result = document.getElementById("result").value;
-    var year = new Date().getFullYear()+parseInt(years);
-   
-    document.addEventListener("DOMContentLoaded", funtion() {
-        document.querySelector("#rate").onchange = function()
-        {
-             var rateval = document.getElementById("rate").value;
-             document.getElementById("rate_val").innerHTML=rateval;
-        }
-    });
-    document.addEventListener("DOMContentLoaded", funtion() {
-        document.querySelector("button").onclick = function(){
-             if( principal <= 0){
-                alert('Enter a positive number');}
-            document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
-        }
-    });
+    var year = new Date().getFullYear();
+    var result_years = parseInt(years)+year;
+    if (principal <= 0) {
+        alert("ENTER POSITIVE NUMBER")
+    } else {
+        document.getElementById("principal").focus()
+        document.getElementById("result").innerHTML="If you deposit <p>"+principal+"</p>,\<br\>at an interest rate of <p> "+rate+"</p>%\<br\>You will receive an amount of <p> "+interest+"</p>,\<br\>in the year <p>"+result_years+"</p>\<br\>"
+    }
     
+}
+
+     
+function updateRate(rate) 
+{
+   
+    document.getElementById("rate_val").innerText=rate;
+
+}
 
         
